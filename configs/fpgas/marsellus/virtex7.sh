df7d47dd56f7e5a970ff433d833dbd35e39f473e
Angelo Garofalo <angelo.garofalo@unibo.it>
2020-03-15 22:31:21 +0100

    add fpga config for pulp & marsellus

---
+++ configs/fpgas/marsellus/virtex7.sh
@@ -0,0 +1,23 @@
+  
+#!/bin/bash -e
+
+export PULPRT_TARGET=marsellus
+export PULPRUN_TARGET=marsellus
+
+if [  -n "${ZSH_VERSION:-}" ]; then 
+    DIR="$(readlink -f -- "${(%):-%x}")"
+    scriptDir="$(dirname $DIR)"
+else
+
+    scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+fi
+
+source $scriptDir/../../common.sh
+
+export PULPRUN_PLATFORM=fpga
+
+#Virtex7 uses 50MHz SoC Frequency
+export PULPRT_CONFIG_CFLAGS='-DARCHI_FPGA_FREQUENCY=50000000'
+
+export io=uart
