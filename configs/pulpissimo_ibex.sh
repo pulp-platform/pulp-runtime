0f63e0b866ceecb86fc2cad8da29dca813c12c6e
Michael Rogenmoser <michael@rogenmoser.us>
2021-02-02 11:16:07 +0100

    Add ibex config scripts

---
+++ configs/pulpissimo_ibex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+export PULPRT_TARGET=pulpissimo
+export PULPRUN_TARGET=pulpissimo
+export USE_IBEX=1
+
+if [  -n "${ZSH_VERSION:-}" ]; then 
+        DIR="$(readlink -f -- "${(%):-%x}")"
+        scriptDir="$(dirname $DIR)"
+else
+
+    scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+fi
+
+source $scriptDir/common.sh
