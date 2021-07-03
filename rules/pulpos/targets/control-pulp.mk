b44cde895bc62a69318c8c648b255d93bfaa9715
bluew <bluewww@users.noreply.github.com>
2021-07-03 22:35:07 +0200

    Add support for control-pulp

---
+++ rules/pulpos/targets/control-pulp.mk
@@ -0,0 +1,57 @@
+ifdef USE_IBEX
+$(error IBEX is not supported in control-pulp)
+endif
+
+# we need at least pulp-gcc v2.1.2
+PULP_LDFLAGS      +=
+PULP_CFLAGS       +=  -D__riscv__
+PULP_ARCH_CFLAGS ?=  -march=rv32imcxcorev -mno-pulp-hwloop
+PULP_ARCH_LDFLAGS ?=  -march=rv32imcxcorev -mno-pulp-hwloop
+PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imcxcorev -mno-pulp-hwloop
+
+PULP_CFLAGS    += -fdata-sections -ffunction-sections \
+	-include chips/pulp/config.h -I$(PULPRT_HOME)/include/chips/control-pulp
+PULP_OMP_CFLAGS    += -fopenmp -mnativeomp
+PULP_LDFLAGS += -nostartfiles -nostdlib -Wl,--gc-sections \
+	-L$(PULPRT_HOME)/kernel -Tchips/control-pulp/link.ld -lgcc
+
+PULP_CC = riscv32-unknown-elf-gcc
+PULP_AR ?= riscv32-unknown-elf-ar
+PULP_LD ?= riscv32-unknown-elf-gcc
+PULP_OBJDUMP ?= riscv32-unknown-elf-objdump
+
+fc/archi=riscv
+pe/archi=riscv
+pulp_chip=control-pulp
+pulp_chip_family=control-pulp
+cluster/version=5
+fc_itc/version=1
+udma/cpi/version=1
+udma/i2c/version=2
+soc/fll/version=1
+udma/i2s/version=2
+udma/uart/version=1
+event_unit/version=3
+perf_counters=True
+fll/version=1
+padframe/version=1
+udma/spim/version=3
+gpio/version=3
+udma/archi=3
+udma/version=3
+soc_eu/version=2
+
+
+# FLL
+PULP_SRCS     += kernel/fll-v$(fll/version).c
+PULP_SRCS     += kernel/freq-domains.c
+PULP_SRCS     += kernel/chips/pulp/soc.c
+
+
+include $(PULPRT_HOME)/rules/pulpos/configs/default.mk
+
+ifeq '$(platform)' 'fpga'
+CONFIG_IO_UART=1
+endif
+
+include $(PULPRT_HOME)/rules/pulpos/default_rules.mk
