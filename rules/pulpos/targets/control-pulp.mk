8720ea32a1ee8088b782d37886dc9536671d1570
aottaviano <aottaviano@iis.ee.ethz.ch>
2021-07-15 11:04:55 +0200

    rules: Fix control-pulp compilation flags

--- rules/pulpos/targets/control-pulp.mk
+++ rules/pulpos/targets/control-pulp.mk
@@ -5,9 +5,9 @@ endif
 # we need at least pulp-gcc v2.1.2
 PULP_LDFLAGS      +=
 PULP_CFLAGS       +=  -D__riscv__
-PULP_ARCH_CFLAGS ?=  -march=rv32imcxcorev -mno-pulp-hwloop
-PULP_ARCH_LDFLAGS ?=  -march=rv32imcxcorev -mno-pulp-hwloop
-PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imcxcorev -mno-pulp-hwloop
+PULP_ARCH_CFLAGS ?=  -march=rv32imcxgap9 -mnohwloop
+PULP_ARCH_LDFLAGS ?=  -march=rv32imcxgap9 -mnohwloop
+PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imcxgap9 -mnohwloop
 
 PULP_CFLAGS    += -fdata-sections -ffunction-sections \
 	-include chips/pulp/config.h -I$(PULPRT_HOME)/include/chips/control-pulp
