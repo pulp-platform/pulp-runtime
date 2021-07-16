9bcaac54a6ef66c5710b238526b32a1693ae06c2
aottaviano <aottaviano@iis.ee.ethz.ch>
2021-07-16 09:26:53 +0200

    rules: Update compiler version and flags for control_pulp

--- rules/pulpos/targets/control-pulp.mk
+++ rules/pulpos/targets/control-pulp.mk
@@ -4,10 +4,10 @@ endif
 
 # we need at least pulp-gcc v2.1.2
 PULP_LDFLAGS      +=
-PULP_CFLAGS       +=  -D__riscv__
-PULP_ARCH_CFLAGS ?=  -march=rv32imcxgap9 -mnohwloop
-PULP_ARCH_LDFLAGS ?=  -march=rv32imcxgap9 -mnohwloop
-PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imcxgap9 -mnohwloop
+PULP_CFLAGS       +=  -D__cv32e40p__ -D__riscv__
+PULP_ARCH_CFLAGS ?=  -march=rv32imfc_xcorev -mno-pulp-hwloop
+PULP_ARCH_LDFLAGS ?=  -march=rv32imfc_xcorev -mno-pulp-hwloop
+PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imfc_xcorev -mno-pulp-hwloop
 
 PULP_CFLAGS    += -fdata-sections -ffunction-sections \
 	-include chips/pulp/config.h -I$(PULPRT_HOME)/include/chips/control-pulp
