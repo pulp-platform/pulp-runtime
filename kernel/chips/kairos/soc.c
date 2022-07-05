43dceff736eed3d0188c774e697cd2292a4296dd
aottaviano <aottaviano@iis.ee.ethz.ch>
2022-07-05 14:40:07 +0200

    pulp-runtime: Set FLL at bring-up for kairos chip target

--- kernel/chips/kairos/soc.c
+++ kernel/chips/kairos/soc.c
@@ -19,11 +19,18 @@
 void pos_soc_init()
 {
 #if __PLATFORM__ != ARCHI_PLATFORM_FPGA
-  pos_freq_domains[PI_FREQ_DOMAIN_FC] = ARCHI_ASIC_FC_FREQUENCY;
-  pos_freq_domains[PI_FREQ_DOMAIN_PERIPH] = ARCHI_ASIC_PER_FREQUENCY;
+
+  pos_fll_constructor();
+
+  pos_freq_domains[PI_FREQ_DOMAIN_FC] = pos_fll_init(POS_FLL_FC);
+
+  pos_freq_domains[PI_FREQ_DOMAIN_PERIPH] = pos_fll_init(POS_FLL_PERIPH);
+
 #else
+
   pos_freq_domains[PI_FREQ_DOMAIN_FC] = ARCHI_FPGA_FC_FREQUENCY;
+
   pos_freq_domains[PI_FREQ_DOMAIN_PERIPH] = ARCHI_FPGA_PER_FREQUENCY;
+
 #endif
 }
-
