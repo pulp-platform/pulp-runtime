13e7333c4b68cae98cca7e8ef25769f5361678b9
Michael Rogenmoser <michael@rogenmoser.us>
2024-02-26 13:51:55 +0100

    Add default platform setting to RTL

--- rules/pulp.mk
+++ rules/pulp.mk
@@ -1,5 +1,6 @@
+platform ?= rtl
 ifdef PULPRUN_PLATFORM
 platform=$(PULPRUN_PLATFORM)
-$(info Setting platform to $(platform))
 endif
+$(info Setting platform to $(platform))
 -include $(PULPRT_HOME)/rules/pulpos/targets/$(PULPRT_TARGET).mk
