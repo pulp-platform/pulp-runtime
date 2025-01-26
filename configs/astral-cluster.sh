27be3f948bbb171d9964eddc3e491808863136d8
Luigi Ghionda <luigi.ghionda2@unibo.it>
2025-01-26 12:47:37 +0100

    Add UART drivers when it is a host peripheral (instead of using udma)
    This mod is tailored for Astral and its host, Cheshire, whose basic functions are reused.

--- configs/astral-cluster.sh
+++ configs/astral-cluster.sh
@@ -4,6 +4,7 @@ export PULPRT_TARGET=astral-cluster
 export PULPRUN_TARGET=astral-cluster
 export CONFIG_NO_FC=1
 export ARCHI_HMR=1
+export ARCHI_HOST_UART=1
 
 if [  -n "${ZSH_VERSION:-}" ]; then
         DIR="$(readlink -f -- "${(%):-%x}")"
