1e3bccf266e52af0943e870f781ffa624df335e8
aottaviano <aottaviano@iis.ee.ethz.ch>
2025-07-09 20:18:15 +0200

    include/archi/chips/carfield: Set default cores to 8

--- include/archi/chips/carfield-cluster/properties.h
+++ include/archi/chips/carfield-cluster/properties.h
@@ -89,7 +89,7 @@
 #define ARCHI_HAS_CLUSTER   1
 #define ARCHI_L1_TAS_BIT    20
 #ifndef ARCHI_CLUSTER_NB_PE
-#define ARCHI_CLUSTER_NB_PE 12
+#define ARCHI_CLUSTER_NB_PE 8
 #endif
 #define ARCHI_NB_CLUSTER    1
 
